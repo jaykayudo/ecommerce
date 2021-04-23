31c5b9ed68d2dfa0fcda13a9484cf79a304579cf
jaykayudo <jaykayudo@gmail.com>
2021-04-23 12:05:23 +0100

    My first commit

--- booktime/settings.py
+++ booktime/settings.py
@@ -108,18 +108,25 @@ WSGI_APPLICATION = 'booktime.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+# 	'default':{
+# 		'ENGINE':'django.db.backends.mysql',
+# 		'NAME':'booktime_db',
+# 		'USER':'jaykay',
+# 		'PASSWORD':'jaykayudo',
+# 		'HOST':'localhost',
+# 		'PORT':'3306',
+# 		'OPTIONS':{
+# 			'init_command':"SET sql_mode = 'STRICT_TRANS_TABLES'"
+# 		}
+# 	}
+# }
+
 DATABASES = {
-	'default':{
-		'ENGINE':'django.db.backends.mysql',
-		'NAME':'booktime_db',
-		'USER':'jaykay',
-		'PASSWORD':'jaykayudo',
-		'HOST':'localhost',
-		'PORT':'3306',
-		'OPTIONS':{
-			'init_command':"SET sql_mode = 'STRICT_TRANS_TABLES'"
-		}
-	}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
